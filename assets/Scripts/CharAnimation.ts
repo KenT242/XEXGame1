2f6267d9c0611d02ffc3ab6da6f76fb75108ff87
uithuyphong <uithuyphong@gmail.com>
2023-11-27 19:14:03 +0700

    add card animation

--- assets/Scripts/CharAnimation.ts
+++ assets/Scripts/CharAnimation.ts
@@ -1,31 +1,34 @@
-import { _decorator, Component, Node, Animation, Enum } from 'cc';
-import { CharMovingType } from './Enums/CharMoving';
+import { _decorator, Component, Node, Animation, Enum } from "cc";
+import { CharMovingType } from "./Enums/CharMoving";
 const { ccclass, property } = _decorator;
 
-@ccclass('CharAnimation')
+@ccclass("CharAnimation")
 export class CharAnimation extends Component {
+  @property({
+    type: Enum(CharMovingType),
+  })
+  public characterMovingType: CharMovingType;
 
+  public anim: Animation = null;
 
-    @property({
-        type: Enum(CharMovingType)
-    })
-    public characterMovingType: CharMovingType
+  onLoad() {
+    this.anim = this.getComponent(Animation);
+    this.anim.play("Idle");
+  }
 
-    public anim: Animation = null;
-
-    onLoad() {
-        this.anim = this.getComponent(Animation)
-        this.anim.play('Idle');
-
+  update(deltaTime: number) {
+    if (this.characterMovingType == CharMovingType.Bet) {
+      if (!this.anim.getState("Card").isPlaying) this.anim.play("Card");
+    } else if (
+      this.characterMovingType == CharMovingType.Idle &&
+      !this.anim.getState("Idle").isPlaying
+    ) {
+      this.anim.play("Idle");
+    } else if (
+      this.characterMovingType == CharMovingType.Walk &&
+      !this.anim.getState("Walk").isPlaying
+    ) {
+      this.anim.play("Walk");
     }
-
-    update(deltaTime: number) {
-        if(!this.anim.getState('Idle').isPlaying && this.characterMovingType == CharMovingType.Idle) {
-            this.anim.play('Idle')
-        }
-        else if (!this.anim.getState('Walk').isPlaying && this.characterMovingType == CharMovingType.Walk) {
-            this.anim.play('Walk')
-        }
-    }
+  }
 }
-
--- assets/Scripts/Enums/CharMoving.ts
+++ assets/Scripts/Enums/CharMoving.ts
@@ -1,7 +1,8 @@
 export enum CharMovingType {
-    Idle =    0,
-    Walk = 1,
-    Attack =   2,
-    Hurt =     3,
-    Die = 4
+  Idle = 0,
+  Walk = 1,
+  Attack = 2,
+  Hurt = 3,
+  Die = 4,
+  Bet = 5,
 }
